28bd8b5058a443696d94778245fa034e3a156a90
Héctor Reyes <hector@dyasoftwaresolutions.onmicrosoft.com>
2019-06-11 23:13:53 -0500

    Semaforos UI

--- FilosofosUI/Form1.Designer.cs
+++ FilosofosUI/Form1.Designer.cs
@@ -28,21 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.F1 = new System.Windows.Forms.PictureBox();
+            this.F4 = new System.Windows.Forms.PictureBox();
+            this.F3 = new System.Windows.Forms.PictureBox();
+            this.F5 = new System.Windows.Forms.PictureBox();
+            this.F2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.F1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.F4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.F3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.F5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.F2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // F1
+            // 
+            this.F1.Image = global::FilosofosUI.Properties.Resources.eating;
+            this.F1.Location = new System.Drawing.Point(283, 12);
+            this.F1.Name = "F1";
+            this.F1.Size = new System.Drawing.Size(124, 132);
+            this.F1.TabIndex = 0;
+            this.F1.TabStop = false;
+            // 
+            // F4
+            // 
+            this.F4.Image = global::FilosofosUI.Properties.Resources.eating;
+            this.F4.Location = new System.Drawing.Point(159, 326);
+            this.F4.Name = "F4";
+            this.F4.Size = new System.Drawing.Size(125, 134);
+            this.F4.TabIndex = 1;
+            this.F4.TabStop = false;
+            // 
+            // F3
+            // 
+            this.F3.Image = global::FilosofosUI.Properties.Resources.eating;
+            this.F3.Location = new System.Drawing.Point(394, 326);
+            this.F3.Name = "F3";
+            this.F3.Size = new System.Drawing.Size(121, 134);
+            this.F3.TabIndex = 2;
+            this.F3.TabStop = false;
+            // 
+            // F5
+            // 
+            this.F5.Image = global::FilosofosUI.Properties.Resources.eating;
+            this.F5.Location = new System.Drawing.Point(79, 141);
+            this.F5.Name = "F5";
+            this.F5.Size = new System.Drawing.Size(124, 134);
+            this.F5.TabIndex = 3;
+            this.F5.TabStop = false;
+            // 
+            // F2
+            // 
+            this.F2.Image = global::FilosofosUI.Properties.Resources.eating;
+            this.F2.Location = new System.Drawing.Point(469, 141);
+            this.F2.Name = "F2";
+            this.F2.Size = new System.Drawing.Size(124, 134);
+            this.F2.TabIndex = 4;
+            this.F2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(671, 472);
+            this.Controls.Add(this.F2);
+            this.Controls.Add(this.F5);
+            this.Controls.Add(this.F3);
+            this.Controls.Add(this.F4);
+            this.Controls.Add(this.F1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.F1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.F4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.F3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.F5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.F2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox F1;
+        private System.Windows.Forms.PictureBox F4;
+        private System.Windows.Forms.PictureBox F3;
+        private System.Windows.Forms.PictureBox F5;
+        private System.Windows.Forms.PictureBox F2;
     }
 }
 
--- FilosofosUI/Form1.cs
+++ FilosofosUI/Form1.cs
@@ -5,21 +5,136 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FilosofosUI
 {
+    public enum Estado
+    {
+        THINKING = 0,
+        HUNGRY = 1,
+        EATING = 2
+    }
+    public class Filosofo
+    {
+        public PictureBox Picture { get; set; }
+        public Estado EstadoF { get; set; }
+    }
     public partial class Form1 : Form
     {
+        const int MAXHILOS = 5;
+        const int N = MAXHILOS;
+        static Semaphore mutex;
+        static List<Semaphore> semaforos = new List<Semaphore>();
+        static List<Thread> hilo = new List<Thread>();
+        static List<Filosofo> filosofos = new List<Filosofo>();
+
         public Form1()
         {
             InitializeComponent();
+
+            Filosofo f1 = new Filosofo();
+            f1.Picture = F1;
+            f1.EstadoF = Estado.THINKING;
+            filosofos.Add(f1);
+
+            Filosofo f2 = new Filosofo();
+            f2.Picture = F2;
+            f2.EstadoF = Estado.THINKING;
+            filosofos.Add(f2);
+
+            Filosofo f3 = new Filosofo();
+            f3.Picture = F3;
+            f3.EstadoF = Estado.THINKING;
+            filosofos.Add(f3);
+
+            Filosofo f4 = new Filosofo();
+            f4.Picture = F4;
+            f4.EstadoF = Estado.THINKING;
+            filosofos.Add(f4);
+
+            Filosofo f5 = new Filosofo();
+            f5.Picture = F5;
+            f5.EstadoF = Estado.THINKING;
+            filosofos.Add(f5);
+
+
+
         }
+
+        private void philosopher(int i)
+        {
+            while (true)
+            {
+                if (i <= 4)
+                {
+                    thinking(i);
+                    take_fork(i);
+                    eat(i);
+                    puts_forks(i);
+                }
+            }
+        }
+
+        private void eat(int v)
+        {
+            Console.WriteLine($"Filosofo {v} comiendo");
+        }
+        private void thinking(int v)
+        {
+            Console.WriteLine($"Filosofo {v} pensando");
+        }
+        private void take_fork(int v)
+        {
+            mutex.WaitOne();
+            filosofos[v].EstadoF = Estado.HUNGRY;
+            filosofos[v].Picture.Image = Properties.Resources.fork_and_knife;
+            test(v);
+            Thread.Sleep(200);
+            mutex.Release();
+            semaforos[v].WaitOne();
+        }
+        private void puts_forks(int v)
+        {
+            mutex.WaitOne();
+            filosofos[v].EstadoF = Estado.THINKING;
+            filosofos[v].Picture.Image = Properties.Resources.thinking;
+            thinking(v);
+            Thread.Sleep(300);
+
+            test((v + N - 1) % N);
+            test((v + 1) % N);
+            mutex.Release();
+        }
+        private void test(int v)
+        {
+            if ((filosofos[v].EstadoF == Estado.HUNGRY) && (filosofos[(v + N - 1) % N].EstadoF != Estado.EATING) && (filosofos[(v + 1) % N].EstadoF != Estado.EATING))
+            {
+                filosofos[v].EstadoF = Estado.EATING;
+                filosofos[v].Picture.Image = Properties.Resources.eating;
+                eat(v);
+                semaforos[v].Release();
+            }
+        }
+      
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            int i;
+            mutex = new Semaphore(1, 2);
+            for (i = 0; i < N; i++)
+                semaforos.Add(new Semaphore(0, 1));
 
+            for (i = 0; i < MAXHILOS; i++)
+            {
+                hilo.Add(new Thread(delegate () { philosopher(i); }));
+                hilo[i].Start();
+            }
+
+            for (i = 0; i < MAXHILOS; i++)
+                hilo[i].Join();
         }
     }
 }
